f04c0524375c76183badeb3cf43eccdc187c8ff8
Roberto Jr <roberto-santos07@hotmail.com>
2023-08-07 11:21:46 -0300

    deploy

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -24,6 +24,17 @@ function About() {
             na década de 60, quando a Letraset lançou decalques contendo
             passagens de Lorem Ipsum, e mais recentemente quando passou a ser
             integrado a softwares de editoração eletrônica como Aldus PageMaker
+          </p>
+          <p>
+            é simplesmente uma simulação de texto da indústria tipográfica e de
+            impressos, e vem sendo utilizado desde o século XVI, quando um
+            impressor desconhecido pegou uma bandeja de tipos e os embaralhou
+            para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não
+            só a cinco séculos, como também ao salto para a editoração
+            eletrônica, permanecendo essencialmente inalterado. Se popularizou
+            na década de 60, quando a Letraset lançou decalques contendo
+            passagens de Lorem Ipsum, e mais recentemente quando passou a ser
+            integrado a softwares de editoração eletrônica como Aldus PageMaker
             <div className="div-button">
               <button type="button">Falar com a Bia</button>
             </div>
