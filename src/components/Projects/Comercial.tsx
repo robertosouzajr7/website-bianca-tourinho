668267f2102d6970a29ab05c5ccbb78e6173075c
Roberto Jr <roberto-santos07@hotmail.com>
2023-08-13 19:39:13 -0300

    feat: add loading

--- src/components/Projects/Comercial.tsx
+++ src/components/Projects/Comercial.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Swiper, SwiperSlide, SwiperRef } from "swiper/react";
 import { Navigation, Pagination } from "swiper/modules";
 import { SwiperOptions } from "swiper/types/swiper-options";
@@ -12,6 +12,13 @@ const Comercial = ({ galeriaComercial }: any) => {
   const [modal, setModal] = useState(false);
   const [id, setId] = useState("");
   const swiperRef = React.useRef<SwiperOptions & SwiperRef>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
 
   const getId = (id: string) => {
     console.log(id);
@@ -29,36 +36,41 @@ const Comercial = ({ galeriaComercial }: any) => {
 
   return (
     <Container>
-      <div className="div-carrosel">
-        {galeriaComercial.map((foto: any) => (
-          <Swiper
-            ref={swiperRef}
-            centeredSlides={true}
-            spaceBetween={1}
-            slidesPerView={"auto"}
-            pagination={{ clickable: true }}
-            modules={[Pagination, Navigation]}
-            className="capa-galeria"
-            onClick={() => getId(foto.id)}
-            autoHeight={true}
-          >
-            <h2>{foto.name}</h2>
-            {foto.fotos.map((imagem: string) => (
-              <SwiperSlide zoom={true} onClick={() => HandleClick(imagem)}>
-                <img src={imagem} alt={imagem} />
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        ))}
-        {modal ? (
-          <Modal
-            HandleCloseModal={HandleCloseModal}
-            selectImage={selectImage2}
-            id={id}
-            galeria={galeriaComercial}
-          />
-        ) : null}
-      </div>
+      {loading ? (
+        <h2>Carregando</h2>
+      ) : (
+        <div className="div-carrosel">
+          {galeriaComercial.map((foto: any) => (
+            <Swiper
+              ref={swiperRef}
+              centeredSlides={true}
+              spaceBetween={1}
+              slidesPerView={"auto"}
+              pagination={{ clickable: true }}
+              modules={[Pagination, Navigation]}
+              className="capa-galeria"
+              onClick={() => getId(foto.id)}
+              autoHeight={true}
+              zoom={true}
+            >
+              <h2>{foto.name}</h2>
+              {foto.fotos.map((imagem: string) => (
+                <SwiperSlide zoom={true} onClick={() => HandleClick(imagem)}>
+                  <img src={imagem} alt={imagem} />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          ))}
+          {modal ? (
+            <Modal
+              HandleCloseModal={HandleCloseModal}
+              selectImage={selectImage2}
+              id={id}
+              galeria={galeriaComercial}
+            />
+          ) : null}
+        </div>
+      )}
     </Container>
   );
 };
